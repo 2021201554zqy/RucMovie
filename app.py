27f82a19d69c386e6ec5c8476f24908a7179e01b
qianyv2003 <zhangqianyu.cn@outlook.com>
2023-11-14 11:42:58 +0800

    Add database support with Flask-SQLAlchemy

--- app.py
+++ app.py
@@ -37,25 +37,49 @@ movies = [
 {'title': 'The Pork of Music', 'year': '2012'},]
 @app.route('/')
 def index():
-    
-    return render_template('index.html', name=name, movies=movies)
+    user = User.query.first() # 读取用户记录
+    movies = Movie.query.all() # 读取所有电影记录
+    return render_template('index.html', user=user, movies=movies)
+
+@app.cli.command()
+def forge():
+    """Generate fake data."""
+    db.create_all()
+# 全局的两个变量移动到这个函数内
+    name = 'Grey Li'
+    movies = [
+    {'title': 'My Neighbor Totoro', 'year': '1988'},
+    {'title': 'Dead Poets Society', 'year': '1989'},
+    {'title': 'A Perfect World', 'year': '1993'},
+    {'title': 'Leon', 'year': '1994'},
+    {'title': 'Mahjong', 'year': '1996'},
+    {'title': 'Swallowtail Butterfly', 'year': '1996'},
+    {'title': 'King of Comedy', 'year': '1999'},
+    {'title': 'Devils on the Doorstep', 'year': '1999'},
+    {'title': 'WALL-E', 'year': '2008'},
+    {'title': 'The Pork of Music', 'year': '2012'},
+    ]
+    user = User(name=name)
+    db.session.add(user)
+    for m in movies:
+        movie = Movie(title=m['title'], year=m['year'])
+        db.session.add(movie)
+    db.session.commit()
+    click.echo('Done.')
+
+# def inject_user(): # 函数名可以随意修改
+#     user = User.query.first()
+#     return dict(user=user) # 需要返回字典，等同于return {'user': user}
 
 
+# @app.errorhandler(404) # 传入要处理的错误代码
+# def page_not_found(e): # 接受异常对象作为参数
+#     user = User.query.first()
+#     return render_template('404.html', user=user), 404 # 返回模板和状态码
 
-def inject_user(): # 函数名可以随意修改
-    user = User.query.first()
-    return dict(user=user) # 需要返回字典，等同于return {'user': user}
 
-
-@app.errorhandler(404) # 传入要处理的错误代码
-def page_not_found(e): # 接受异常对象作为参数
-    user = User.query.first()
-    return render_template('404.html', user=user), 404 # 返回模板和状态码
-
-@app.context_processor
 @app.cli.command() # 注册为命令
-@click.option('--drop', is_flag=True, help='Create after drop.')
-# 设置选项
+@click.option('--drop', is_flag=True, help='Create after drop.')#设置选项
 def initdb(drop):
     """Initialize the database."""
     if drop: # 判断是否输入了选项
